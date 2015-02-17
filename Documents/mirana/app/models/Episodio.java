93809a2b143aa5e948a67786d46f612ce0692688
ocdnecro <blancos2004@yahoo.com.mx>
2015-02-16 21:53:50 -0500

    CRUD

--- Documents/mirana/app/models/Episodio.java
+++ Documents/mirana/app/models/Episodio.java
@@ -12,7 +12,10 @@ public class Episodio extends Model {
 	Atributos
 	*/
 	@Id
-	public String id;
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	public long id;
+
+	public String idPaciente;
 
 	public Paciente paciente;
 
@@ -22,10 +25,89 @@ public class Episodio extends Model {
 
 	public String descipción;
 
+	public String lugar;
+
 	
 	/*
 	Constructor
 	*/
-	public Episodio()
+	public Episodio(String pId, Paciente pPaciente, Date pFecha, int pIntensidad, String pDescripcion, String pLugar )
+	{
+		//id = pId;
+		paciente = pPaciente;
+		fecha = pFecha;
+		intensidad = pIntensidad;
+		descipción = pDescripcion;
+		lugar = pLugar;
+		
+	}
+
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public Paciente getPaciente()
+	{
+		return paciente;
+	}
+
+	public void setPaciente(pPaciente)
+	{
+		paciente = pPaciente;
+	}
+
+	public Date getFecha()
+	{
+		return fecha;
+	}
+
+	public void setFecha(Date pFecha)
+	{
+		fecha = pFecha;
+	}
+
+	public int getIntencidad()
+	{
+		return intensidad;
+	}
+
+	public void setIntesidad(int pIntensidad)
+	{
+		intensidad = pIntensidad;
+	}
+
+	public String getDescripcion()
+	{
+		return descipción;
+	}
+
+	public void setDescripcion(Sitring pDesc)
+	{
+		descipción = pDesc;
+	}
+
+	public String getLugar()
+	{
+		return lugar;
+	}
+
+	public void setLugar(String pLugar)
+	{
+		lugar = pLugar;
+	}
+
+	
+
+
+
+
+
+
+
+
+
+
 
 }
