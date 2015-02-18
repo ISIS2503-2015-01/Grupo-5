602e5eea936888cb984f8951473df699706ecb77
lfvargas1746 <luisfvargas@gmail.com>
2015-02-17 19:23:35 -0500

    catalizador terminado, falta servicios

--- Mirana/app/models/Catalizadores.java
+++ Mirana/app/models/Catalizadores.java
@@ -7,33 +7,75 @@ import javax.persistence.Id;
 @Entity
 public class Catalizadores extends Model {
 
+	//-------------------------------------
+	//         Atributos
+	//-------------------------------------
+
 	/*
-	Atributos
+	El identificador del catalizador, es un valor único para cada uno.
 	*/
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
+    /*
+    Nombre asociado al catalizador
+    */
 	public String nombre;
 
+	/*
+	Medicamento que se debe tomar para aliviar los sintomas
+	*/
 	public String medicamento;
 
+	/*
+	Recomendación sobre algún tipo de tratamiento alternativa
+	*/
 	public String alternativo;
 
+	/*
+	Se debe o no guardar reposo
+	*/
 	public boolean reposo;
 
+	/*
+	Se debe o no realizar dieta
+	*/
 	public boolean dieta;
 
+	/*
+	Se debe o no realizar terapiá física
+	*/
 	public boolean terapia;
 
+	/* 
+	Episodio asociado a el catalizador
+	*/
+	private Long episodioID;
+
+	//-------------------------------------
+	//         Constructores
+	//-------------------------------------
+
 	/*
-	Constructor 
+	Constructor sin parámetros
 	*/
 	public Catalizadores(){
 
 	}
 
-	public static Catalizadores create( String nombree, String medicamentoo, String alternativoo, boolean reposoo, boolean dietaa, boolean terapiaa){
+	/*
+	Constructor con parámetros:
+	nombree: el nombre del catalizador
+	medicamentoo: el medicamento asociado 
+	alternativoo: el tratamiento alternativo propuesta
+	reposoo: si se debe descansar
+	dietaa: si se debe seguir una dieta
+	terapiaa: si se debe realizar algún tipo de terapia
+	episodioid: el episodio con el que esta conectado el catalizador
+	Retorna el nuevo catalizador
+	*/
+	public static Catalizadores create( String nombree, String medicamentoo, String alternativoo, boolean reposoo, boolean dietaa, boolean terapiaa, Long episodioid){
 
 	Catalizadores catalizador = new Catalizadores();
 
@@ -43,20 +85,32 @@ public class Catalizadores extends Model {
 	catalizador.reposo = reposoo;
 	catalizador.dieta = dietaa;
 	catalizador.terapia = terapiaa;
+	catalizador.episodioID = episodioid;
+
+	return catalizador;
 	}
 
-	/*
-	Métodos
-	*/
+	//-------------------------------------
+	//         Métodos
+	//-------------------------------------
 
+	/*
+	Retorna el nombre del catalizador
+	*/
 	public String darNombre(){
 		return nombre;
 	}
 
+	/*
+	Cambia el nombre del catalizador
+	*/
 	public void cambiarNombre(String nuevo){
 		this.nombre = nuevo;
 	}
 
+	/*
+	Retorna el medicamento propuesto
+	*/
 	public String darMedicamento(){
 		return medicamento;
 	}
@@ -65,4 +119,70 @@ public class Catalizadores extends Model {
 		this.medicamento = nuevo;
 	}
 
+	/*
+	Retorna el tratamiento alternativo propuesto
+	*/
+	public String darAlternativo(){
+		return alternativo;
+	}
+
+	public void cambiarAlternativo(String nuevo){
+		this.alternativo = nuevo;
+	}
+
+	/*
+	Retorna si se debe guardar reposo
+	*/
+	public boolean darReposo(){
+		return reposo;
+	}
+
+	/*
+	Cambia el reposar o no
+	*/
+	public void cambiarReposo(boolean nuevo){
+		this.reposo = nuevo;
+	}
+
+	/*
+	Retorna si se debe hacer dieta
+	*/
+	public boolean darDieta(){
+		return dieta;
+	}
+
+	/*
+	Cambia el hacer o no dieta
+	*/
+	public void cambiarDieta(boolean nuevo){
+		this.dieta = nuevo;
+	}
+
+	/*
+	Retorna si se debe hacer terapia
+	*/
+	public boolean darTerapia(){
+		return terapia;
+	}
+
+	/*
+	Cambia el hacer o no terapia
+	*/
+	public void cambiarTerapia(boolean nuevo){
+		this.terapia = nuevo;
+	}
+
+	/*
+	Retorna el episodio asociado al catalizador
+	*/
+	public Long darEpisodio(){
+		return episodioID;
+	}
+
+	/*
+	Cambia el episodio asociado al catalizador
+	*/
+	public void cambiarEpisodio(Long nuevo){
+		this.episodioID = nuevo;
+	}
 }
