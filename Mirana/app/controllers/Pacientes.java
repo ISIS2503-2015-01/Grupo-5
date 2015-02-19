1a1d219e9658ffd9d4366aca12687907f15af5a1
joalri24 <joalri24@hotmail.com>
2015-02-18 20:35:19 -0500

    Metí el Delete en pacientes

--- Mirana/app/controllers/Pacientes.java
+++ Mirana/app/controllers/Pacientes.java
@@ -20,4 +20,10 @@ public class pacientesController extends Controller
 		List<Paciente> pacientes= new Model.Finder(String.class, Paciente.class).all();
 		return ok(toJson(pacientes));
 	}
-}
+	
+	 public static Result deletePaciente()
+	 {
+		String id="1";
+		Paciente.find.ref(id).delete();
+		return redirect(routes.Application.index());
+	}
