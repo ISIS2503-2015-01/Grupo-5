57c5600ab3bf736c284db3d241d67ed28d10b992
Jose Alejandro Ria�o <ja.riano913@uniandes.edu.co>
2015-02-18 20:18:02 -0500

    Agregué el controlador de pacientes

---
+++ Mirana/app/controllers/Pacientes.java
@@ -0,0 +1,17 @@
+package controllers;
+import models.Paciente;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.*;
+/**
+* Created by USUARIO on 17/02/2015.
+*/
+public class pacientesController extends Controller
+{
+	public static Result addPaciente()
+	{
+		Paciente paciente = Form.form(Paciente.class).bindFromRequest().get();
+		paciente.save();
+		return redirect(routes.Application.index());
+	}
+}
